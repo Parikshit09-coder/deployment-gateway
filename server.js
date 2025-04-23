1874ce7adb3ba6db6b8a59ce1f9305b4a1ad9917
Parikshit09-coder <gurudesai2005@gmail.com>
2025-04-23 09:49:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT;
 
 
 
-app.use('/node_server', proxy('process.env.NODE_URL'));
-app.use('/RAG_server', proxy('process.env.RAG_URL'));
+app.use('/node_server', proxy(process.env.NODE_URL));
+app.use('/RAG_server', proxy(process.env.RAG_URL));
 
 
 app.listen(PORT, "0.0.0.0",() => {
