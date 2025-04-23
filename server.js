6b4bb7d039c148385b9674c5118f3c90858230f5
Parikshit09-coder <gurudesai2005@gmail.com>
2025-04-23 09:43:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT;
 
 
 
-app.use('/node_server', proxy('https://deployment-production-4d20.up.railway.app'));
-app.use('/RAG_server', proxy('https://medpass-rag.onrender.com'));
+app.use('/node_server', proxy('process.env.NODE_URL'));
+app.use('/RAG_server', proxy('process.env.RAG_URL'));
 
 
 app.listen(PORT, "0.0.0.0",() => {
