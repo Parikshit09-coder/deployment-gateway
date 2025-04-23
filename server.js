5a9439c49fa1e93f03114d76ed66038e3e49f462
Your Name <you@example.com>
2025-04-23 09:27:44 +0530

    hello

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const proxy = require ('express-http-proxy')
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+
+app.use('/node_server', proxy('https://deployment-production-4d20.up.railway.app'));
+app.use('/RAG_server', proxy('https://medpass-rag.onrender.com'));
+
+
+app.listen(PORT, "0.0.0.0",() => {
+    console.log(`Server is running on port ${PORT}`);
+})
